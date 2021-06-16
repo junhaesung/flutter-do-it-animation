731c98aa51cba91e0fa92a5d767152e8b83601e6
Haeseong Jeon <seong0428@gmail.com>
2021-06-17 02:35:15 +0900

    그래프 애니메이션 - peopleList 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'people.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -21,6 +23,20 @@ class AnimationApp extends StatefulWidget {
 }
 
 class _AnimationApp extends State<AnimationApp> {
+  final List<People> peopleList = [];
+  int current = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    peopleList.add(People('스미스', 180, 92));
+    peopleList.add(People('메리', 162, 55));
+    peopleList.add(People('존', 177, 75));
+    peopleList.add(People('바트', 130, 40));
+    peopleList.add(People('콘', 194, 140));
+    peopleList.add(People('디디', 100, 80));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
